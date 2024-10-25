5673350ebfb64a35b63b962dff1677383a33a0ef
Paulius Juzenas <59730296+Paulijuz@users.noreply.github.com>
2024-10-25 16:35:35 +0200

    feat: add arcade stearing mode

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,13 @@ XboxSeriesXControllerESP32_asukiaaa::Core controller(CONTROLLER_ADDRESS);
 
 unsigned long last_notification_received_at = 0;
 
+typedef enum steering_mode_e {
+  TANK,
+  ARCADE,
+} steering_mode_t;
+
+static steering_mode_t steering_mode = TANK;
+
 void setup() {
   Serial.begin(115200);
 
@@ -31,7 +38,7 @@ void loop() {
 
   Serial.print("\33[2K\r");
   controller.onLoop();
-
+  
   if (!controller.isConnected()) {
     Serial.print("Connection lost.");
     
@@ -65,9 +72,39 @@ void loop() {
 
   Serial.print(" Recieved at: ");
   Serial.print(last_notification_received_at);
-
-  weapon_set_speed((float)controller.xboxNotif.trigRT / CONTROLLER_TRIGGER_MAX);
   
-  motor_set_speed(LEFT, -2*(float)controller.xboxNotif.joyLVert / CONTROLLER_JOY_MAX + 1);
-  motor_set_speed(RIGHT, -2*(float)controller.xboxNotif.joyRVert / CONTROLLER_JOY_MAX + 1);
+  if (controller.xboxNotif.btnDirUp) {
+    steering_mode = ARCADE;
+  }
+
+  if (controller.xboxNotif.btnDirDown) {
+    steering_mode = TANK;
+  }
+
+  weapon_set_speed(((float)controller.xboxNotif.trigLT + (float)controller.xboxNotif.trigRT) / (2 * CONTROLLER_TRIGGER_MAX));
+  
+  float left_ver = -2*(float)controller.xboxNotif.joyLVert / CONTROLLER_JOY_MAX + 1;
+  float left_hor = -2*(float)controller.xboxNotif.joyLHori / CONTROLLER_JOY_MAX + 1;
+  float right_ver = -2*(float)controller.xboxNotif.joyRVert / CONTROLLER_JOY_MAX + 1;
+  
+  switch (steering_mode) {
+  case TANK:
+    motor_set_speed(LEFT, left_ver);
+    motor_set_speed(RIGHT, right_ver);
+    break;
+
+  case ARCADE:
+    // For arcade steering, use right stick vertical for forward/backward
+    // and left stick horizontal for turning.
+    float left_speed = right_ver - left_hor;  // Adjust left motor speed based on turning
+    float right_speed = right_ver + left_hor; // Adjust right motor speed based on turning
+
+    // Ensure speeds stay within the -1 to 1 range
+    left_speed = max(-1.0f, min(1.0f, left_speed));
+    right_speed = max(-1.0f, min(1.0f, right_speed));
+
+    motor_set_speed(LEFT, left_speed);
+    motor_set_speed(RIGHT, right_speed);
+    break;
+  }
 }
